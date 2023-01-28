143a319061f8a6b8d149641b889ccf3f5ffcff15
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-28 12:52:24 -0500

    importing component views from album and artist

---
+++ src/components/AlbumView.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'; 
+
+function AlbumView () {
+const [albumData, setAlbumData] = useState([])
+
+return (
+    <div>
+        <p>Artist Data Goes Here!</p>
+    </div>
+    )
+}
+
+export default AlbumView

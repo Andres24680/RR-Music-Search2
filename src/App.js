2289abf70a9bc4466425f2a9284c5c8e56db1de4
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-28 12:24:24 -0500

    completed step 5 useref

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
-import {useEffect, useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
-import {SearchContext} from './context/SearchContext'
-
+import { SearchContext } from './context/SearchContext'
 
 function App() {
-	let [searchTerm, setSearchTerm] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
-	let searchInput = useRef('')
+    let [message, setMessage] = useState('Search for Music!')
+ let [data, setData] = useState([])
+ let searchInput = useRef('')
 
-	//const API_URL = 'https://itunes.apple.com/search?term='
-
-	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
-		}
-	}, [search])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
-
-	return (
-		<div className="App">
-			<SearchContext.Provider value={{
-				term: searchInput,
-				handleSearch:handlesearch
-			}}>
-				<SearchBar />
-				</SearchContext.Provider>
-				{message}
-				<DataContext.Provider value={data}>
-					<Gallery />
-				</DataContext.Provider>
-		</div>
-  	);
+    const API_URL = 'https://itunes.apple.com/search?term='
+    
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        // Fetch Data
+        const fetchData = async () => {
+            document.title = `${term} Music`
+            const response = await fetch(API_URL + term)
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+                // Set State and Context value
+                return setData(resData.results)
+            } else {
+                return setMessage('Not Found')
+            }
+        }
+        fetchData()
+    }
+    
+    return (
+        <div className="App">
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+                }}>
+                <SearchBar />
+            </SearchContext.Provider>
+            {message}
+            <DataContext.Provider value={data}>
+                <Gallery />
+            </DataContext.Provider>
+        </div>
+    );
 }
 
 export default App;
+
+

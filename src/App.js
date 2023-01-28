6bc3ada52542d3f71dccded844e39bd05fbc0917
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-28 12:12:26 -0500

    adding searchBar contexts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
+import {SearchContext} from './context/SearchContext'
+
 
 function App() {
-	let [search, setSearch] = useState('')
+	let [searchTerm, setSearchTerm] = useState('')
 	let [message, setMessage] = useState('Search for Music!')
 	let [data, setData] = useState([])
+	let searchInput = useRef('')
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+	//const API_URL = 'https://itunes.apple.com/search?term='
 
 	useEffect(() => {
 		if(search) {
 			const fetchData = async () => {
 				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
+				const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
 				const resData = await response.json()
 				if (resData.results.length > 0) {
 					setData(resData.results)
@@ -32,12 +35,17 @@ function App() {
 	}
 
 	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+		<div className="App">
+			<SearchContext.Provider value={{
+				term: searchInput,
+				handleSearch:handlesearch
+			}}>
+				<SearchBar />
+				</SearchContext.Provider>
+				{message}
+				<DataContext.Provider value={data}>
+					<Gallery />
+				</DataContext.Provider>
 		</div>
   	);
 }
